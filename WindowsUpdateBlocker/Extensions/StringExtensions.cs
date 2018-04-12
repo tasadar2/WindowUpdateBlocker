208d256db7926d92378d95cceb908022ef4ee0b5
tasadar2 <tassadar@wowway.com>
2018-04-12 01:01:43 -0400

    Initial program

---
+++ WindowsUpdateBlocker/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsUpdateBlocker.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Indent(this string value, string indent = "    ")
+        {
+            return value.Replace("\n", "\n" + indent);
+        }
+    }
+}
---
+++ WindowsUpdateBlocker/Program.cs
@@ -0,0 +1,32 @@
+﻿using Topshelf;
+
+namespace WindowsUpdateBlocker
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(config =>
+            {
+                config.Service<WindowsUpdateBlockerService>(sc =>
+                {
+                    sc.ConstructUsing(() => new WindowsUpdateBlockerService());
+
+                    sc.WhenStarted(s => s.Start());
+                    sc.WhenStopped(s => s.Stop());
+                });
+
+                config.SetDisplayName("Windows Update Blocker");
+                config.SetServiceName("WindowsUpdateBlocker");
+                config.SetDescription("Blocks windows updates from running.");
+                config.RunAsLocalSystem();
+                config.StartAutomatically();
+
+                config.EnableServiceRecovery(rc =>
+                {
+                    rc.RestartService(1);
+                });
+            });
+        }
+    }
+}
